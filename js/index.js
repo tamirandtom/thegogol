9166a84e74ea6224d0be5d91e86cda5ade203ddc
Tamir Pomerantz <tamir.pomerantz@tomera-lap.similar.il>
2018-08-20 09:05:43 +0300

    added animations and transitions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
-var App = angular.module('gogol', ['ngAnimate']);
-App.controller('index', ['$scope', '$http', '$location', function ($scope, $http, $location, $interval) {
 
+
+var winReactions = ['😀','😍','👻','😺','👍','👏','✌️','🤟','🎩','🕶','🦄','🐳','🍄']
+var loseReactions = ['🐓','🐣','😾','👎','🍎']
+var App = angular.module('gogol', ['ngAnimate']);
+App.controller('index', ['$scope', '$http', '$location', '$timeout', function ($scope, $http, $location, $timeout) {
+    var queueTimeOut = 1500;
     $scope.data = [{
         name: 'tamir',
         age: '30',
@@ -40,8 +44,14 @@ App.controller('index', ['$scope', '$http', '$location', function ($scope, $http
         img: 'http://foo.jpg'
     }];
 
+    $scope.gameState = {
+        currState:'wait'
+    };
 
+    
 $scope.charecterQueue = [];
+
+$scope.emojiStack = [];
 
 $scope.currQuestion = $scope.data[0];
 $scope.currQuestionPos = 0;
@@ -51,39 +61,70 @@ $scope.initGame = function() {
     $scope.charecterQueue.push($scope.data[0]);  
     $scope.charecterQueue.push($scope.data[1]); 
 $scope.currQuestionPos = 1;
+$scope.gameState.currState = 'wait';
 }
 
 $scope.nextMove = function() {
+    console.log('nextmove');
     $scope.currQuestionPos++;
     $scope.charecterQueue.push($scope.data[$scope.currQuestionPos++]);  
+    $scope.gameState.currState = 'wait';
 }
 
+$scope.verdict = function(v) {
+    $scope.emojiStack=[];
 
+if (v=='win')
+{
+    $scope.gameState.currState = 'winScreen';
 
-$scope.countto = function(num) {
-    $scope.charecterQueue[$scope.charecterQueue.length-1]=0;
+    // move forawrd
+    $timeout( function(){
+        $scope.nextMove();
+    }, queueTimeOut );
 
-    $interval(function () {
-        if ( $scope.charecterQueue[$scope.charecterQueue.length-1].age < num)
-        {
-        $scope.charecterQueue[$scope.charecterQueue.length-1].age++;
+    // emoji waterfall
+    for(var i=0;i<30;i++) {
+        $timeout (function(){
+            $scope.emojiStack.push(winReactions[Math.floor(Math.random() * winReactions.length)]);
+        }, 200*i);
     }
-    }, 1000);
+    
+
+
+}    else {
+
+    $scope.gameState.currState = 'loseScreen';
+    for(var i=0;i<30;i++) {
+        $timeout (function(){
+            $scope.emojiStack.push(loseReactions[Math.floor(Math.random() * loseReactions.length)]);
+        }, 200*i);
+    }
+
+    // end the game
+        $timeout( function(){
+    $scope.endGame();
+        }, queueTimeOut );
+
+} 
 }
 
 $scope.checkWin = function(more) {
+    $scope.gameState.currState = 'halt';
     if (($scope.charecterQueue[$scope.charecterQueue.length-1].age > $scope.charecterQueue[$scope.charecterQueue.length-2].age && more == 'more') || ($scope.charecterQueue[$scope.charecterQueue.length-1].age <= $scope.charecterQueue[$scope.charecterQueue.length-2].age && more == 'less')) {        
-        console.log('win!');
-
-        $scope.nextMove();
+        $scope.verdict('win');
     } else {
-        console.log('lose');
-        $scope.nextMove();
+        $scope.verdict('lose');
     }
 
 }
 
 
+$scope.endGame = function() { 
+//TODO
+$scope.gameState.currState = 'end';
+
+}
 
 $scope.initGame();
 
